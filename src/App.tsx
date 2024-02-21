34e5496cd69c9070e0c2cbdcebf4911bc2c58463
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-02-21 11:39:28 -0500

    Completed Route Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, About } from "./pages/index";
+
+import { Home, About, Contact} from "./pages/index";
 import "./App.css";
 
-
+import Navbar from "./components/Navbar";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Resume />} />
       </Routes>
     </Router>
   );
