3086cc3e534b7d8d46daa6abf55d62507817017e
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-02-23 10:58:04 -0500

    Fixed Unused Variable Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import PageNotFound from "./pages/404";
 
 function App() {
